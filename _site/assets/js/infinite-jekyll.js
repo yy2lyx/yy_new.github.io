d84bb3b872ad6dcae08fe0769a591ac4627a3d28
yy2lyx <1282442333@qq.com>
2026-01-12 23:22:15 +0800

    fix

--- _site/assets/js/infinite-jekyll.js
+++ _site/assets/js/infinite-jekyll.js
@@ -9,16 +9,46 @@ $(function() {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   
+  // Calculate postsToLoad first, before using it
+  var postsToLoad = $(".tag-master:not(.hidden) .post-list").children().length,
+      loadNewPostsThreshold = 10;
+  
   // If a tag was passed as a url parameter then use it to filter the urls
   if (urlParams.has('tag')){
-    const tag = urlParams.get('tag');
-    document.getElementById(tag).classList.toggle('hidden');
+    const tag = decodeURIComponent(urlParams.get('tag'));
+    
+    // Find the tag element using jQuery selector
+    const tagElement = $('#' + tag.replace(/[!"#$%&'()*+,.\/:;<=>?@[\\\]^`{|}~]/g, '\\$&'));
+    
+    // Check if the tag element exists before trying to show it
+    if (tagElement.length > 0) {
+      tagElement.removeClass('hidden');
+      // Recalculate postsToLoad after showing the tag
+      postsToLoad = $(".tag-master:not(.hidden) .post-list").children().length;
+    } else {
+      // If tag element not found, log for debugging and disable fetching
+      console.warn('Tag element not found for tag:', tag);
+      disableFetching();
+      return;
+    }
+    
     $.getJSON('./posts-by-tag.json', function(data) {
         let tag_item = data.find(el => el.tag === tag);
-        postURLs = tag_item["posts"];
-        // If there aren't any more posts available to load than already visible, disable fetching
-        if (postURLs.length <= postsToLoad)
-        disableFetching();
+        if (tag_item && tag_item["posts"]) {
+          postURLs = tag_item["posts"];
+          // If there aren't any more posts available to load than already visible, disable fetching
+          if (postURLs.length <= postsToLoad) {
+            disableFetching();
+          }
+        } else {
+          // If tag not found in JSON, disable fetching
+          console.warn('Tag not found in posts-by-tag.json:', tag);
+          disableFetching();
+        }
+    }).fail(function() {
+      // If JSON file fails to load, disable fetching
+      console.error('Failed to load posts-by-tag.json');
+      disableFetching();
     });
   } else {
       $.getJSON('./all-posts.json', function(data) {
@@ -26,11 +56,11 @@ $(function() {
         // If there aren't any more posts available to load than already visible, disable fetching
         if (postURLs.length <= postsToLoad)
           disableFetching();
+      }).fail(function() {
+        // If JSON file fails to load, disable fetching
+        disableFetching();
       });
   }
-
-  var postsToLoad = $(".tag-master:not(.hidden) .post-list").children().length,
-      loadNewPostsThreshold = 10;
 
   // If there's no spinner, it's not a page where posts should be fetched
   if ($(".spinner").length < 1)
